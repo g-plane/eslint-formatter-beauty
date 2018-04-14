f627a03c194a1564ee4f7b8d2beae1aec073ea11
Pig Fang <g-plane@hotmail.com>
2018-04-14 09:51:57 +0800

    feat: errors come first

--- index.js
+++ index.js
@@ -25,68 +25,71 @@ module.exports = results => {
     output += chalk.underline(result.filePath.replace(process.cwd(), '.'))
     output += '\n'
 
-    result.messages.forEach(message => {
-      output += message.severity === 2
-        ? chalk.red(`  Error: ${message.message} `)
-        : chalk.yellow(`  Warning: ${message.message} `)
-      output += chalk.gray(`(${message.ruleId})`)
-      output += '\n'
+    result
+      .messages
+      .sort((a, b) => b.severity - a.severity)
+      .forEach(message => {
+        output += message.severity === 2
+          ? chalk.red(`  Error: ${message.message} `)
+          : chalk.yellow(`  Warning: ${message.message} `)
+        output += chalk.gray(`(${message.ruleId})`)
+        output += '\n'
 
-      const rawLines = getLines(message.line)
-      const padding = rawLines[rawLines.length - 1].line.toString().length + 6
-      const lines = rawLines.map(line => {
-        let painted = chalk.gray(padStart(
-          `${line.line} | `,
-          padding
-        ))
+        const rawLines = getLines(message.line)
+        const padding = rawLines[rawLines.length - 1].line.toString().length + 6
+        const lines = rawLines.map(line => {
+          let painted = chalk.gray(padStart(
+            `${line.line} | `,
+            padding
+          ))
 
-        const painter = text => (message.severity === 2
-          ? chalk.bgRed(chalk.white(text))
-          : chalk.bgYellow(chalk.black(text)))
-        painter.open = `/*****${message.severity === 2
-          ? style.bgRed.open + style.white.open
-          : style.bgYellow.open + style.black.open}`
-        painter.close = `${message.severity === 2
-          ? style.bgRed.close + style.white.close
-          : style.bgYellow.close + style.black.close}*****/`
+          const painter = text => (message.severity === 2
+            ? chalk.bgRed(chalk.white(text))
+            : chalk.bgYellow(chalk.black(text)))
+          painter.open = `/*****${message.severity === 2
+            ? style.bgRed.open + style.white.open
+            : style.bgYellow.open + style.black.open}`
+          painter.close = `${message.severity === 2
+            ? style.bgRed.close + style.white.close
+            : style.bgYellow.close + style.black.close}*****/`
 
-        if (line.line === message.line) {
-          const start = message.column
-          const end = message.endColumn || start
+          if (line.line === message.line) {
+            const start = message.column
+            const end = message.endColumn || start
 
-          const chars = line.value.split('')
-          chars.splice(start - 1, 0, painter.open)
-          if (message.line === message.endLine || !message.endLine) {
-            chars.splice(start === end ? end + 1 : end, 0, painter.close)
-          } else {
-            chars.push(painter.close)
-          }
+            const chars = line.value.split('')
+            chars.splice(start - 1, 0, painter.open)
+            if (message.line === message.endLine || !message.endLine) {
+              chars.splice(start === end ? end + 1 : end, 0, painter.close)
+            } else {
+              chars.push(painter.close)
+            }
 
-          painted += highlight(chars.join(''))
-        } else if (message.endLine
+            painted += highlight(chars.join(''))
+          } else if (message.endLine
           && line.line > message.line
           && line.line < message.endLine) {
-          painted += highlight(`/*****${painter(line.value)}*****/`)
-        } else if (message.endLine && line.line === message.endLine) {
-          const chars = line.value.split('')
-          chars.unshift(painter.open)
-          chars.splice(message.endColumn, 0, painter.close)
+            painted += highlight(`/*****${painter(line.value)}*****/`)
+          } else if (message.endLine && line.line === message.endLine) {
+            const chars = line.value.split('')
+            chars.unshift(painter.open)
+            chars.splice(message.endColumn, 0, painter.close)
 
-          painted += highlight(chars.join(''))
-        } else {
-          painted += highlight(line.value)
-        }
+            painted += highlight(chars.join(''))
+          } else {
+            painted += highlight(line.value)
+          }
 
-        painted = painted.replace(/\/\*\*\*\*\*/g, '')
-          .replace(/\*\*\*\*\*\//g, '')
+          painted = painted.replace(/\/\*\*\*\*\*/g, '')
+            .replace(/\*\*\*\*\*\//g, '')
 
-        return painted
+          return painted
+        })
+
+        output += lines.join('\n')
+
+        output += '\n\n'
       })
-
-      output += lines.join('\n')
-
-      output += '\n\n'
-    })
   })
 
   const errorsCount = results
