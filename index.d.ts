85f8b25a00184fbf6a318b46481540771852b7eb
Pig Fang <g-plane@hotmail.com>
2018-04-15 10:10:40 +0800

    fix: fix declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 import * as eslint from 'eslint'
 
-function formatter (results: eslint.CLIEngine.LintResult[]): string
+declare function formatter (results: eslint.CLIEngine.LintResult[]): string
 
 export = formatter
